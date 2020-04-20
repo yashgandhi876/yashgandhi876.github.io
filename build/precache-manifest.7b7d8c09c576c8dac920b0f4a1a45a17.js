08b27492c518c548da723d631bea63c906e9805d
Yash Gandhi <yashgandhi2020@gmail.com>
2020-04-21 02:54:39 +0530

    add

---
+++ build/precache-manifest.7b7d8c09c576c8dac920b0f4a1a45a17.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62bdc704ba4adccbe841d86b015717d",
+    "url": "/yashgandhi.tech/index.html"
+  },
+  {
+    "revision": "bf8226eaa311a6cab6dc",
+    "url": "/yashgandhi.tech/static/css/main.d7f5c056.chunk.css"
+  },
+  {
+    "revision": "dac92547a88d3566ef80",
+    "url": "/yashgandhi.tech/static/js/2.309a854a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yashgandhi.tech/static/js/2.309a854a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf8226eaa311a6cab6dc",
+    "url": "/yashgandhi.tech/static/js/main.4070af66.chunk.js"
+  },
+  {
+    "revision": "5c21a051f3104db3e80f",
+    "url": "/yashgandhi.tech/static/js/runtime-main.8ec2e737.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yashgandhi.tech/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ff6e9861b18def17a66d06c43239317a",
+    "url": "/yashgandhi.tech/static/media/netlify.ff6e9861.png"
+  }
+]);
