87f153dd4b911ab09cb847736a4b7815a33a0f3e
Yash Gandhi <yashgandhi2020@gmail.com>
2020-05-02 17:24:22 +0530

    Updates

---
+++ precache-manifest.e5d63cfc2d38e111cad5b6350d68a59d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fac46d11a346e32959e90a22137077c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b66bb29dc02b56955bf",
+    "url": "/static/css/main.b3559121.chunk.css"
+  },
+  {
+    "revision": "e1b3e00e6ae2993a3a2d",
+    "url": "/static/js/2.ce90b559.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce90b559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b66bb29dc02b56955bf",
+    "url": "/static/js/main.105459c4.chunk.js"
+  },
+  {
+    "revision": "62d82772d23bf4232b7a",
+    "url": "/static/js/runtime-main.862b1ddb.js"
+  },
+  {
+    "revision": "ba5e141297e4646e63f648567e2cb1ae",
+    "url": "/static/media/Cal.ba5e1412.jpg"
+  },
+  {
+    "revision": "69d9ec80a92b376c455baf1fbcaa5825",
+    "url": "/static/media/antakshari.69d9ec80.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "ae33ec56255a173b4908b387b142a884",
+    "url": "/static/media/mes1.ae33ec56.jpg"
+  },
+  {
+    "revision": "1796288fe97d7065da99e24f802a185d",
+    "url": "/static/media/netlify.1796288f.png"
+  },
+  {
+    "revision": "d3362463551ed77ab33522e308592b0d",
+    "url": "/static/media/portfolio.d3362463.jpg"
+  },
+  {
+    "revision": "c4d2c204f716377bd15631e4bbb7ba60",
+    "url": "/static/media/s_l.c4d2c204.jpg"
+  },
+  {
+    "revision": "4086e6b7077ef2f9d8f95631b7495845",
+    "url": "/static/media/tic_tac_toe.4086e6b7.jpg"
+  }
+]);
