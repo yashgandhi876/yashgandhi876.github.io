b0d779d60730ae72aaacc23b42ba0c07db7960f0
Yash Gandhi <yashgandhi2020@gmail.com>
2020-06-20 16:42:45 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc16552a7360c61628210269e8e57c45.js"
+  "/precache-manifest.2defbfb62b471ef8ed144c9b273c1b84.js"
 );
 
 self.addEventListener('message', (event) => {
